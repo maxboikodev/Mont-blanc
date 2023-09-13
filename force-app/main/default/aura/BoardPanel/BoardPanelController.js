d2b0682c44f93f6d799a1c7b852d5aa3d1fecb06
Maks <93492986+taximarseille@users.noreply.github.com>
2023-09-13 20:25:39 +0300

    aura added

--- force-app/main/default/aura/BoardPanel/BoardPanelController.js
+++ force-app/main/default/aura/BoardPanel/BoardPanelController.js
@@ -8,17 +8,12 @@
 
         // update selectedMode attribute
         component.set("v.selectedMode", selectedValue);
-        //console.log("The start new game button is clicked. The game mode is" + selectedValue);
-        
-        let x = component.find ("testValueId");
-        let x1 = x.get ("v.value");
-        component.set ("v.testValueAttr", x1); 
+        console.log("The start new game button is clicked. The game mode is" + selectedValue);
 
-
-        //console.log("Selected mode attribute value: " + component.get("v.selectedMode"));
+        console.log("Selected mode attribute value: " + component.get("v.selectedMode"));
     },
 
     reshuffleBoard: function (component, event, helper) {
-        //console.log("Reshuffle board is called");
+        console.log("Reshuffle board is called");
     }
 });
