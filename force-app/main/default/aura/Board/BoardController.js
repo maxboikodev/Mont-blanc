e2a37368857e77b68fb72812f71a8c9d61e75002
Max Boiko <boikom001@gmail.com>
2023-09-15 15:24:36 +0300

    dynamic board

--- force-app/main/default/aura/Board/BoardController.js
+++ force-app/main/default/aura/Board/BoardController.js
@@ -2,8 +2,26 @@
     doInit: function (component, event, helper) {
         console.log("Initialiation completed");
         // build a list of 100 words
-        const words = helper.getWords(6);
+
+    let mode = component.get ("v.mode");
+        let wordsqnt;
+        let itemsize; 
+
+        if (mode == "hard"){ 
+            wordsqnt = 36;
+            itemsize = 2;
+            } else if (mode == "medium") {
+                wordsqnt = 24;
+                itemsize = 3;
+                } else {
+                    wordsqnt = 12;
+                    itemsize = 4;
+            }
+
+        component.set ("v.Itemsize", itemsize);
+        const words = helper.getWords(wordsqnt);
         component.set("v.words", words);
+
         console.log("Words: " + words);
         // get win word
         const winWord = helper.getWinWord(words);
